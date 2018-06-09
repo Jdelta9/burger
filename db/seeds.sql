6b45a16d41439bcfb6718acf2a3600561874acf7
Jessica Cervantes <J1123C@gmail.com>
2018-06-08 23:48:30 -0700

    addittion of folders

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Double Bacon Jalapeño Cheeseburger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('grilled onion Cheeseburger Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Grilled Cheeseburger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Black bean Veggie Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Jack daniels buffalo burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Turkey avocado Burger', true);
